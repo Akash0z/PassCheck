9c6312f74fe546ab9a92d36a8016e69a540cce5d
Akash Goswami <akashgoswami112006@gmail.com>
2026-08-17 12:43:14 +0530

    Added error handling for unhandled routes

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.set('view engine', 'ejs');
 
 app.use(mainRouter);
 
+app.use((req, res) => {
+  res.status(404).render("error");
+});
+
 const PORT = 3000;
 app.listen(PORT , (req, res) => {
   console.log(`Server running on http://localhost:${PORT}`);
