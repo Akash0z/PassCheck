c5aad27bbd9b566b4d2577b2f2a2d0bf357d5dca
Akash Goswami <akashgoswami112006@gmail.com>
2026-08-17 12:51:00 +0530

    Refactored code

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use((req, res) => {
   res.status(404).render("error");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT , (req, res) => {
   console.log(`Server running on http://localhost:${PORT}`);
 })
